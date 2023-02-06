9356c7b6c506ccee7f4ada3c02d77bafa7448734
engageintellect <jc9361@gmail.com>
2023-02-05 17:49:37 -0800

    init

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+search_term='sveltekit'
+
 def search_youtube(query, max_results=25):
-<<<<<<< HEAD
     API_KEY = os.getenv("YOUTUBE_API_KEY")
-=======
-    API_KEY = <YOUR API KEY?
->>>>>>> 86580b9072f0d0d4c784be4ce6824905d661a008
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={query}&type=video&key={API_KEY}&maxResults={max_results}&order=date"
     response = requests.get(url)
     results = response.json()["items"]
     return results
 
-results = search_youtube("sveltekit")
+results = search_youtube(search_term)
 for result in results:
     print(result["snippet"]["publishedAt"], result["snippet"]["title"])
 
