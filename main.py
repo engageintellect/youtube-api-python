e6f159513c1452a05a19a7d95858519fdfa8af42
engageintellect <jc9361@gmail.com>
2023-02-05 17:45:29 -0800

    abstract api key

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-search_term = 'turbo sveltekit'
+load_dotenv()
 
 def search_youtube(query, max_results=25):
-    API_KEY = "AIzaSyCSis_YjZDjyGgNvXSZPaYiZ-3oZ165SNo"
+    API_KEY = os.getenv("YOUTUBE_API_KEY")
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={query}&type=video&key={API_KEY}&maxResults={max_results}&order=date"
     response = requests.get(url)
     results = response.json()["items"]
     return results
 
-results = search_youtube(search_term)
+results = search_youtube("sveltekit")
 for result in results:
     print(result["snippet"]["publishedAt"], result["snippet"]["title"])
 
