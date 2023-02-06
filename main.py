86580b9072f0d0d4c784be4ce6824905d661a008
engage_intelllect <61082194+engageintellect@users.noreply.github.com>
2023-02-05 17:43:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 search_term = 'turbo sveltekit'
 
 def search_youtube(query, max_results=25):
-    API_KEY = "AIzaSyCSis_YjZDjyGgNvXSZPaYiZ-3oZ165SNo"
+    API_KEY = <YOUR API KEY?
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={query}&type=video&key={API_KEY}&maxResults={max_results}&order=date"
     response = requests.get(url)
     results = response.json()["items"]
